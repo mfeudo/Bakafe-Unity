9fcd20dd15dd6cdf3487ca3b96b0a3318e81a439
MariaFeudo <mfeudo@ucsc.edu>
2020-11-13 23:23:57 -0800

    Added a decision example and temp art
    At the end of the scene there is now an example of what a decision could look like. Also added temp art for a textbox and background

--- Bakafe/Assets/Scripts/TextBoxManager.cs
+++ Bakafe/Assets/Scripts/TextBoxManager.cs
@@ -8,6 +8,11 @@ public class TextBoxManager : MonoBehaviour{
     //the textbox in game
     public GameObject textbox;
     
+    //the textboxes for the choice options
+    public Text choiceOneTextbox;
+    public Text choiceTwoTextBox;
+
+
     public Text theText;
 
     public TextAsset textFile; 
@@ -23,13 +28,27 @@ public class TextBoxManager : MonoBehaviour{
 
 //button to advance text
     public Button continueButton;
+
+//buttons for choices
+
+    public Button choiceOneButton;
+    public Button choiceTwoButton;
     
+//flag for if a choice is being made, an int so we can track and pull from muiltiple choices
+
+    public int choice;
     // Start is called before the first frame update
     void Start()
     {
 
-    Button btn = continueButton.GetComponent<Button>();
-		btn.onClick.AddListener(ContinueButtonClick);
+    Button btnContinue = continueButton.GetComponent<Button>();
+	btnContinue.onClick.AddListener(ContinueButtonClick);
+
+    Button btnOptionOne = choiceOneButton.GetComponent<Button>();
+	btnOptionOne.onClick.AddListener(OptionOneButtonClick);
+
+    Button btnOptionTwo = choiceTwoButton.GetComponent<Button>();
+	btnOptionTwo.onClick.AddListener(OptionTwoButtonClick);
 
     //sort all of the text into the text file array separating by new line characters, there is probably a better/smarter way to do this that we can think of 
         if (textFile != null){
@@ -37,9 +56,15 @@ public class TextBoxManager : MonoBehaviour{
         }
         
 
-        if(endAtLine == 0){
-            endAtLine = textLines.Length - 1;
-        }
+        //if(endAtLine == 0){
+            //endAtLine = textLines.Length - 1;
+        //}
+
+        currentLine=1;
+        endAtLine =24;
+
+        choiceOneTextbox.text = "";
+        choiceTwoTextBox.text = "";
     }
 
     // Update is called once per frame
@@ -51,6 +76,27 @@ public class TextBoxManager : MonoBehaviour{
             currentLine++;
         }
 
+        //check for the first decision
+        if(currentLine == 24){
+            choice = 1;
+        }
+
+        if(choice==1){
+            continueButton.gameObject.SetActive(false);
+            choiceOneTextbox.text = textLines[25];
+            choiceTwoTextBox.text = textLines[26];
+            choiceOneButton.gameObject.SetActive(true);
+            choiceTwoButton.gameObject.SetActive(true);
+
+        }
+        //triggers if a choice is not currently happening
+        if(choice ==0){
+            choiceOneButton.gameObject.SetActive(false);
+            choiceTwoButton.gameObject.SetActive(false);
+            continueButton.gameObject.SetActive(true);
+           
+        }
+
     }
 
     void ContinueButtonClick(){
@@ -58,5 +104,30 @@ public class TextBoxManager : MonoBehaviour{
             currentLine++;
         }
         
+    }
+
+    void OptionOneButtonClick(){
+        if(choice == 1 ){
+            currentLine = 27;
+            endAtLine = 28;
+        }
+
+        //reset all of the UI
+        choice = 0;
+        choiceOneTextbox.text = "";
+        choiceTwoTextBox.text = "";
+    }
+
+    void OptionTwoButtonClick(){
+        if(choice==1){
+            currentLine = 29;
+            endAtLine = 30;
+        }
+
+         //reset all of the UI
+        choice = 0;
+        choiceOneTextbox.text = "";
+        choiceTwoTextBox.text = "";
+            
     }
 }
