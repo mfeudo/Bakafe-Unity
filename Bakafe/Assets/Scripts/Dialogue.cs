d5519e87a684aad7de6dd57a1464cb3de31f312a
MariaFeudo <mfeudo@ucsc.edu>
2020-11-13 21:20:50 -0800

    Text file combatibility added
    A textbox now pulls from a textfile in a linear order, with pressing enter to advance the dialogue.

---
+++ Bakafe/Assets/Scripts/Dialogue.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Dialogue {
+
+    public string name;
+    public string[] sentences;
+    // Start is called before the first frame update
+    void Start(){
+
+    }
+    
+}
---
+++ Bakafe/Assets/Scripts/TextBoxManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextBoxManager : MonoBehaviour{
+
+    //the textbox in game
+    public GameObject textbox;
+    
+    public Text theText;
+
+    public TextAsset textFile; 
+
+ //an array that will hold all of the dialogue   
+    public string [] textLines; 
+
+//indicates the line number that is currently being displayed on screen
+    public int currentLine; 
+//indicates the line number that the text should stop at, for either a choice or end of dialogue
+    public int endAtLine; 
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    //sort all of the text into the text file array separating by new line characters, there is probably a better/smarter way to do this that we can think of 
+        if (textFile != null){
+            textLines = (textFile.text.Split('\n'));
+        }
+        
+
+        if(endAtLine == 0){
+            endAtLine = textLines.Length - 1;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        theText.text = textLines[currentLine];
+
+        if(Input.GetKeyDown(KeyCode.Return)&&(currentLine<endAtLine)){
+            currentLine++;
+        }
+
+    }
+}
