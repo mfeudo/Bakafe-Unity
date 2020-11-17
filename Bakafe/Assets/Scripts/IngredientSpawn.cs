a8284172f0baea9ce59b8e859b90da466ef7a8b9
Daniel Jarreau Ortega <dhortega@ucsc.edu>
2020-11-17 00:21:57 -0800

    Created Crafting Testing Scene
    Created a new scene for creating a prototype of a crafting system.

---
+++ Bakafe/Assets/Scripts/IngredientSpawn.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IngredientSpawn : MonoBehaviour
+{
+    Vector2 worldPosition;
+    GameObject cube;
+    
+    public void Start() {
+        worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        cube = GameObject.Find("Cube");
+    }
+
+    public void createIngredient() {
+        Instantiate(cube, worldPosition, Quaternion.identity);
+    }
+}
